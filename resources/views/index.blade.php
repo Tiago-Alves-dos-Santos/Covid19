418f9624c4820c6e1b0b0cfdbd5af6e18f7a203f
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-06-22 15:05:40 -0300

    content center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,17 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" type="image/x-icon">
     <title>Covid-19</title>
+    {{-- CSS --}}
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    {{-- DataTable --}}
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    {{-- FIM CSS --}}
+
+    {{-- JS --}}
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+
+    {{-- DataTable --}}
+    <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    {{-- FIM JS --}}
 </head>
 <body>
     <img src="{{ asset('img/favicon.png') }}" id="movingImage" alt="">
-    <script>
-        var movingImage = $('#movingImage');
 
+    <div class="content-center">
+        <div class="content">
+
+        </div>
+    </div>
+
+<script>
+
+    function animateImage() {
+        var movingImage = $('#movingImage');
         var windowWidth = $(window).width();
         var windowHeight = $(window).height();
-
         var imageWidth = movingImage.width();
         var imageHeight = movingImage.height();
 
@@ -25,23 +43,27 @@
         var dx = 5; // Velocidade horizontal
         var dy = 5; // Velocidade vertical
 
-        function animateImage() {
-            x += dx;
-            y += dy;
+        function moveImage() {
+        x += dx;
+        y += dy;
 
-            if (x + imageWidth >= windowWidth || x <= 0) {
-                dx = -dx;
-            }
-
-            if (y + imageHeight >= windowHeight || y <= 0) {
-                dy = -dy;
-            }
-
-            movingImage.css({ 'left': x, 'top': y });
-
-            requestAnimationFrame(animateImage);
+        if (x + imageWidth >= windowWidth || x <= 0) {
+            dx = -dx;
         }
-        animateImage();
-    </script>
+
+        if (y + imageHeight >= windowHeight || y <= 0) {
+            dy = -dy;
+        }
+
+        movingImage.css({ 'left': x, 'top': y });
+
+        requestAnimationFrame(moveImage);
+        }
+
+        moveImage();
+    }
+
+    animateImage();
+</script>
 </body>
 </html>
